ac4960398f90554951848cef1c31d7d6c4dda162
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-04-07 10:17:23 +0000

    [Add]サインイン時のエラーメッセージ

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_user, only: [:create]
 
   # GET /resource/sign_in
   def new
@@ -24,4 +25,19 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def reject_user
+      @user = User.find_by(email: params[:user][:email])
+      if @user
+        if @user.valid_password?(params[:user][:password]) && (@user.deleted == true)
+          flash[:left_notice] = "退会しています　再度ご登録をしてご利用ください"
+          redirect_to new_user_registration_path
+        else
+          flash[:left_notice] = "入力内容を確認してください"
+        end
+      else
+          flash[:left_notice] = "入力内容を確認してください"
+      end
+  end
+
 end
