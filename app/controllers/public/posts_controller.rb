0df12d2da2a7ace2be0c27f377c85c9638250227
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-04-08 02:18:44 +0000

    [Update]コントローラ

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,14 +1,16 @@
 class Public::PostsController < ApplicationController
-  
+  before_action :set_user, only: [ :new, :show, :create, :confirm ]
+
   def new
     @post = Post.new
   end
 
   def show
-    
+    @post = Post.find(params[:id])
   end
 
   def create
+    post = Pst
   end
 
   def destroy
@@ -16,11 +18,15 @@ class Public::PostsController < ApplicationController
 
   def confirm
   end
-  
+
   private
-  
+
+  def set_user
+    @user = current_user
+  end
+
   def post_params
     params.require(:post).permit(:posted_on, :body)
   end
-  
+
 end
