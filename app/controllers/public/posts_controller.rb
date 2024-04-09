54f4a22cc3391d94801b966859ca0e6da725b67c
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-04-09 11:54:16 +0000

    [Update]コントローラ、アクセス制限

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,4 +1,5 @@
 class Public::PostsController < ApplicationController
+  before_action :is_matching_login_user, only: [:destroy]
 
   def new
     @post = Post.new
@@ -23,12 +24,22 @@ class Public::PostsController < ApplicationController
 
   def confirm
     @post = Post.new(post_params)
+    if @post.posted_on.blank?||@post.body.blank?
+      redirect_to new_post_path, notice: "設定されていない項目があります"
+    end
   end
 
   private
 
   def post_params
     params.require(:post).permit(:posted_on, :body)
+  end
+
+  def is_matching_login_user
+    post = Post.find(params[:id])
+    unless post.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 
 end
