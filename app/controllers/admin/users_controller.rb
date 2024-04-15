46ec327e8888e56ff2e24f8fb338a484543255b6
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-04-15 10:33:07 +0000

    [Update]ルーティング

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,10 +4,24 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    @user = User.find_by!(custom_identifier: params[:id])
     if @user.nil?
       redirect_to admin_users_path
       return
     end
   end
+
+  def update
+    user = User.find_by!(custom_identifier: params[:id])
+    if user.deleted == false
+      user.update(deleted: true)
+      redirect_to admin_user_custom_id_path(user)
+      return
+    else
+      user.update(deleted: false)
+      redirect_to admin_user_custom_id_path(user)
+      return
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,9 @@ Rails.application.routes.draw do
   # 管理者側
   namespace :admin do
     resources :users, only: [:index, :show, :update]
+    # get 'id/:custom_identifier', to: 'users#show', as: 'user_custom_id'
+    # patch 'users/:custom_identifier', to: 'users#update', as: 'user'
+
     resources :posts, only: [:index, :show, :destroy]
   end
 
