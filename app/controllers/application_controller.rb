f4f341e1a2e5120c2127f56a2e3d49d3e0267332
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-04-15 07:46:48 +0000

    [Update]サインイン遷移先,アクセス制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :about, :search]
+  before_action :authenticate_admin!, except: [:top, :about, :search]
 
 end
