1a56835ccae5b37c0b616be3cdeb49a177a6b7f8
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-04-19 09:07:30 +0000

    [Update]migrate修正

--- db/migrate/20240419054916_create_relationships.rb
+++ db/migrate/20240419054916_create_relationships.rb
@@ -3,7 +3,7 @@ class CreateRelationships < ActiveRecord::Migration[6.1]
     create_table :relationships do |t|
       t.integer :follower_id, null: false
       t.integer :followed_id, null: false
-      t.boolean :approved, null: false, default: false
+      t.boolean :approved, null: false, default: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(version: 2024_04_19_054916) do
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id", null: false
     t.integer "followed_id", null: false
-    t.boolean "approved", default: false, null: false
+    t.boolean "approved", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
