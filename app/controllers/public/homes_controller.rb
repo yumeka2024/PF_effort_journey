51f03433259fd1ca2cd76c259851e8c7eb4a2d10
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-04-22 18:49:36 +0000

    [Update]Issues対応 close #76

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,3 +1,4 @@
+# app/controllers/public/homes_controller.rb
 class Public::HomesController < ApplicationController
 
   def top
@@ -6,6 +7,7 @@ class Public::HomesController < ApplicationController
       @user = current_user
       @approved_followers = @user.followers.where('relationships.approved = ?', true)
       @approved_following = @user.followings.where('relationships.approved = ?', true)
+      @follow_posts = Post.joins(user: { active_relationships: :follower }).where(relationships: { approved: true, follower_id: @user.id }).includes(user: { image_attachment: :blob }).page(params[:page]).per(5).order(created_at: :desc)
     end
   end
 
