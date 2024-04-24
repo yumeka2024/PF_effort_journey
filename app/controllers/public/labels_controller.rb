bcc926816f1427586622877b18804dceefe0c030
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-04-24 09:59:49 +0000

    [Update]labelsコントローラ

--- app/controllers/public/labels_controller.rb
+++ app/controllers/public/labels_controller.rb
@@ -1,15 +1,43 @@
 class Public::LabelsController < ApplicationController
 
   def index
+    @label = Label.new
+    @labels = Label.all
+  end
+
+  def edit
+    @label = Label.find(params[:id])
   end
 
   def create
+    @label = Label.new(label_params)
+    if @label.save
+      redirect_to labels_path, notice: "保存しました"
+    else
+      @labels = Label.all
+      render 'index'
+    end
   end
 
   def update
+    @label = Label.find(params[:id])
+    if @label.update(label_params)
+      redirect_to labels_path, notice: "保存しました"
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    label = Label.find(params[:id])
+    label.destroy
+    redirect_to labels_path, flash: { center_notice: '削除しました' }
+  end
+
+  private
+
+  def label_params
+    params.require(:label).permit(:genre, :name)
   end
 
 end
