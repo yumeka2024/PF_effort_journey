b30fe86b1e5a31c032ab0538daa5a0a1d62ad333
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-05-03 06:27:46 +0000

    [Add]scorカラム,コントローラ記述

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -32,6 +32,7 @@ class Public::PostsController < ApplicationController
 
   def create
     post = current_user.posts.new(post_params)
+    post.score = Language.get_data(post_params[:body])
     post.save
     redirect_to root_path, flash: { center_notice: '投稿が完了しました' }
   end
