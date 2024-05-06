356383d9a548ec04fc98d4331a0b0ec3b9899260
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-05-06 12:04:12 +0000

    [Fix]コメント削除時のリダイレクト先

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -7,7 +7,7 @@ class Admin::CommentsController < ApplicationController
   def destroy
     comment = Comment.find(params[:id])
     comment.destroy
-    redirect_back fallback_location: root_path
+    redirect_back fallback_location: admin_comments_path
   end
 
 end
