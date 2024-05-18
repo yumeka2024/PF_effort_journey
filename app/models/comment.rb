5f8b0ecdd56ee8b30b7afa53e0b87b7d9f010b15
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-05-18 14:15:26 +0000

    [Fix]最終確認、修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,8 +7,4 @@ class Comment < ApplicationRecord
 
   validates :body, presence: true
 
-  # after_commit on: :create do
-  #   create_notification(user_id: post.user_id, post_id: post_id, sender_id: user_id, message: 4)
-  # end
-
 end
